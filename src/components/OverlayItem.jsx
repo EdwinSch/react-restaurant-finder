c6f69b38894f8e68b4450cdf463aacdaa21e8243
EdwinSch <edwinschutjes@gmail.com>
2024-01-19 15:53:27 +0100

    add: gps info

--- src/components/OverlayItem.jsx
+++ src/components/OverlayItem.jsx
@@ -1,10 +1,17 @@
 import RestaurantInfo from "./RestaurantInfo";
+import { IoNavigateCircle } from "react-icons/io5";
 
 const OverlayItem = ({ item }) => {
   return (
     <div className="overlay-wrapper">
       <img src={item.image} alt="restaurant image" />
       <RestaurantInfo item={item} />
+
+      {/* GPS */}
+      <div className="gps-wrapper">
+        <IoNavigateCircle className="gps-icon" />
+        <p>{item.gps} km</p>
+      </div>
     </div>
   );
 };
